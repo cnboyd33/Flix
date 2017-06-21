a5188673a6ae4b0180fdce995249f710213a34a3
Cameryn Boyd <cboyd7@fb.com>
2017-06-21 15:32:21 -0700

    completed app image, launch screen image, + network request

---
+++ Flix/NowPlayingViewController.swift
@@ -0,0 +1,53 @@
+//
+//  NowPlayingViewController.swift
+//  Flix
+//
+//  Created by Cameryn Boyd on 6/21/17.
+//  Copyright © 2017 Cameryn Boyd. All rights reserved.
+//
+
+import UIKit
+
+class NowPlayingViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: <#T##URL#>) { (data, respnse, error) in
+            
+            //this will run when the network request returns
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data {
+                let dataDictionary = try!JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                let movies = dataDictionary["results"] as! [[String: Any]]
+                for movie in movies {
+                    let title = movie["title"] as! String
+                    print(title)
+                }
+            }
+        }
+        task.resume()
+
+        
+    }
+
+   
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    
+
+}
