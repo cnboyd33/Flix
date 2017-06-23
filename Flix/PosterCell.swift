0f752f697b364b10926d56d183b083ac3592ed1d
Cameryn Boyd <cboyd7@fb.com>
2017-06-23 15:12:44 -0700

    added collection view

---
+++ Flix/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix
+//
+//  Created by Cameryn Boyd on 6/23/17.
+//  Copyright © 2017 Cameryn Boyd. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
